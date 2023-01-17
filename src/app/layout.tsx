2506790222d26f738efeae5f2c831e35a2137227
ishaf <shafiulislam20@gmail.com>
2023-01-17 15:35:47 +0600

    init 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,21 @@
-import './globals.css'
-
+import 'antd/es/style/reset.css'
+import React from "react";
+async function getData() {
+    const res = await fetch('https://api.mindwords.info/post/this-is-a-title', {
+        cache: "force-cache",
+        next: {revalidate: 30}
+    })
+    return res.json()
+}
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+        <head/>
+            <body>{children}</body>
+        </html>
+    )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
+
+import AntDTheme from "@/components/antdTheme";
+
 async function getData() {
     const res = await fetch('https://api.mindwords.info/post/this-is-a-title', {
         cache: "force-cache",
         next: {revalidate: 30}
     })
-  return res.json()
+    return res.json()
 }
-
 export default async function Home() {
-  const post = await getData()
+    const post = await getData()
     return (
-        <div>{post.data.details}</div>
+        <AntDTheme>
+            <div>{post.data.details}</div>
+        </AntDTheme>
     )
 }
